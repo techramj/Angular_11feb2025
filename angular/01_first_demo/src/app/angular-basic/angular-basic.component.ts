dba3455a76d20f23bd4025007998745734e69592
techramj <79793143+techramj@users.noreply.github.com>
2025-02-24 21:15:53 +0530

    pipe

--- angular/01_first_demo/src/app/angular-basic/angular-basic.component.ts
+++ angular/01_first_demo/src/app/angular-basic/angular-basic.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { GenderPipe } from '../shared/pipes/gender.pipe';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-angular-basic',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, GenderPipe],
   templateUrl: './angular-basic.component.html',
   styleUrl: './angular-basic.component.css'
 })
@@ -19,7 +21,30 @@ export class AngularBasicComponent {
   show = true;
   hide = false;
 
-  time = new Date();
+  time = new Observable<string>(s => {
+    setInterval(()=>{
+      s.next(new Date().toLocaleString())
+    })
+  });
+
+  emp = {
+    id : 101,
+    name: 'RaM JAIswaL',
+    salary: '98765434210.4',
+    variable : 0.25,
+    doj : new Date(),
+    mobile: '9876543210',
+    pan: 'abcd123401',
+    isMarried: true,
+    city: {name:'Pune', temperature:'29.3', lattitude:"10", longitude:"73.5"},
+    gender: 1
+  }
+
+  showEmployeeDetails = true;
+
+  showHide(){
+    this.showEmployeeDetails = !this.showEmployeeDetails;
+  }
 
 
 }
